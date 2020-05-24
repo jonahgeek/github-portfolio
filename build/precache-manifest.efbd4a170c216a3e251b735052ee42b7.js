ed7d2d60f91343f147b11e23d163146775b1bf70
jonahgeek <ajon3210@gmail.com>
2020-05-24 09:01:55 +0300

    innitial Commit

---
+++ build/precache-manifest.efbd4a170c216a3e251b735052ee42b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d2c43c3dfdd7acb8ff872f1e6d14ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1ed77a16153f155782b",
+    "url": "/static/js/2.b3be0bdb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b3be0bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06782997c8018435666",
+    "url": "/static/js/main.d4bde1c9.chunk.js"
+  },
+  {
+    "revision": "46bd9b732bfe233bbf4b",
+    "url": "/static/js/runtime-main.6a435a0b.js"
+  }
+]);
